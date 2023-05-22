29f6c9395a81c561b4f7876717b05f8d986acfdb
Sameer Bhamla <71896224+sameerbhamla@users.noreply.github.com>
2023-05-22 10:57:23 -0400

    Finished Day1

---
+++ Day1/main.py
@@ -0,0 +1,2 @@
+print("Welcome to the band name generator!")
+print("Your band name could be " + input("Whats the name of the city you grew up in?: ") + " " + input("What is the name of your pet?: "))
