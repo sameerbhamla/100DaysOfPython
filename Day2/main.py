86dcd62adcdd5f153e12da36e3153d67ea9589b4
Sameer Bhamla <71896224+sameerbhamla@users.noreply.github.com>
2023-05-30 11:54:33 -0400

    Final Commit

--- Day2/main.py
+++ Day2/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+print("welcome to the tip calculator")
+total = input("What was the total bill? ")
+tipPercent = input("What percentage tip would you like to give? 10, 12, or 15? ")
+tipPercent = (float(tipPercent) + 100) / 100
+numOfPeople = input("How many people to split the bill? ")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+amtPerPerson = (float(total) / int(numOfPeople)) * tipPercent
+amtPerPerson = round(amtPerPerson,2)
+print(f"Each person should pay: ${amtPerPerson}")
