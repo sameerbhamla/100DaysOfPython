28d10ea4d509f4f9b0988025139fc905cbe95ed8
Sameer Bhamla <71896224+sameerbhamla@users.noreply.github.com>
2023-06-02 09:47:59 -0400

    Finished Day5

--- Day5/main.py
+++ Day5/main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+#Password Generator Project
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+           'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password = []
 
+for i in range(0,nr_letters):
+    password += random.choice(letters)
+for i in range(0, nr_symbols):
+    password += random.choice(symbols)
+for i in range(0, nr_numbers):
+    password += random.choice(numbers)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+random.shuffle(password)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Your password is: ", end="")
+for i in range(0, len(password)):
+    print(password[i], end="")
