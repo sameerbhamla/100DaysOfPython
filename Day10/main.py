dd8d5b5a20364365896fe31d2948eb936a8cb30c
Sameer Bhamla <71896224+sameerbhamla@users.noreply.github.com>
2023-06-10 00:28:56 -0400

    Day10 Finished

--- Day10/main.py
+++ Day10/main.py
@@ -1,6 +1,50 @@
-def format_name(f_name, l_name):
-    print(f_name.title())
-    print(l_name.title())
+import art
+
+print(art.logo)
 
 
-format_name("anGeLa", "yU")
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    num1 = float(input("Whats the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    is_continuing = True
+
+    while is_continuing:
+        operation_symbol = input("pick an operation from the line above: ")
+        num2 = float(input("Whats the second number: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type n to exit.:  ").lower() == 'y':
+            num1 = answer
+        else:
+            is_continuing = False
+            calculator()
+
+
+calculator()
