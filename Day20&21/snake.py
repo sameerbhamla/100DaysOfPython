452383de72e8271768c9cc4389e8c2ff9d311835
Sameer Bhamla <71896224+sameerbhamla@users.noreply.github.com>
2023-11-05 17:56:41 -0500

    F

--- Day20&21/snake.py
+++ Day20&21/snake.py
@@ -48,3 +48,21 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(0)
+
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+
+
+    def add_segment(self,position):
+        new_segment = Turtle(shape='square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
